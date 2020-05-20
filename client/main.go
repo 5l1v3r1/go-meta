b3777af189c5dea9a492515d9b21419840b3cc0b
obehi.iredia@gmail.com <obehi.iredia@gmail.com>
2020-05-20 17:57:21 -0400

    Add main

--- client/main.go
+++ client/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-meta/rpc"
+)
+
+func main() {
+
+	// Add host/pass as environment variables, do not hardcode this!
+	host := os.Getenv("ADD an ENV variable")
+	pass := os.Getenv("ADD an ENV variable")
+	user := "msf"
+
+	if host == "" || pass == "" {
+		log.Println("Missing required environment variables")
+	}
+	msf, err := rpc.New(host, user, pass)
+	if err != nil {
+		log.Println(err)
+	}
+	defer msf.Logout()
+
+	sess, err := msf.SessionList()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("Sessions: ")
+	for _, session := range sess {
+		fmt.Printf("%5d %s\n", session.ID, session.Info)
+	}
+}
